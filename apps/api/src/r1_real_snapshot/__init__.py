74214dc850f17dcb55864a55a73e21e64500d7e4
brianstewart377-rgb <brianstewart377@gmail.com>
2026-08-31 14:11:33 +0100

    feat: expose R1 real snapshot API

---
+++ apps/api/src/r1_real_snapshot/__init__.py
@@ -0,0 +1,4 @@
+from .loader import SnapshotSafetyError,assert_read_only,load_snapshots,read_db_identity
+from .report import build_report
+from .types import SnapshotSelector
+__all__=['SnapshotSafetyError','SnapshotSelector','assert_read_only','build_report','load_snapshots','read_db_identity']
