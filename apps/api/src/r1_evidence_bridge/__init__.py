47e42702ccd37343edfe2d818b17f4fef58ce15a
brianstewart377-rgb <brianstewart377@gmail.com>
2026-08-31 14:06:14 +0100

    feat: expose R1 evidence bridge API

---
+++ apps/api/src/r1_evidence_bridge/__init__.py
@@ -0,0 +1,4 @@
+from .body_projection import project_body
+from .candidate_projection import project_candidate,project_system
+from .slot_prediction import predict_orbital_capacity,predict_surface_slots
+__all__=['project_body','project_candidate','project_system','predict_orbital_capacity','predict_surface_slots']
