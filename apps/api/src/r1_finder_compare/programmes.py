6e01a7c6bc153b0d2c742ea9f2a569635b32cadc
brianstewart377-rgb <brianstewart377@gmail.com>
2026-08-31 13:33:14 +0100

    feat: define R1 Finder proof programme contracts

---
+++ apps/api/src/r1_finder_compare/programmes.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+PROGRAMME_ID = 'P-ER-01'
+TEMPLATE_REVISION = 'p-er-01-finder-proof-2026-08-31.1'
+ROLE_ID = 'role_extraction_v1'
+ROLE_REVISION = 'role-extraction-finder-proof-2026-08-31.1'
+STRATEGY_ID = 'bounded_geometric_v1'
+STRATEGY_REVISION = 'finder-proof-2026-08-31.1'
+
+ER_REQUIREMENTS = (
+    'ER-PLACE-01',
+    'ER-EXT-01',
+    'ER-REF-01',
+    'ER-PAIR-01',
+    'ER-ALLOC-01',
+    'ER-LOG-01',
+    'ER-EVID-01',
+)
+
+EX_REQUIREMENTS = (
+    'EX-EVID-01',
+    'EX-SOURCE-01',
+    'EX-CAP-01',
+    'EX-LOG-01',
+)
